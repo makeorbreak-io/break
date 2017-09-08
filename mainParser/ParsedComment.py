cbbc72b020f2230723acfa5be5ff545f2a77cd66
Jose Martins <lluismmartins7@gmail.com>
2017-09-08 13:44:16 +0100

    starting mainParser development

---
+++ mainParser/ParsedComment.py
@@ -0,0 +1,5 @@
+class ParsedComment:
+	def __init__(self, action, assignees, comment):
+		self.action 	= action;
+		self.assignees 	= assignees;
+		self.comment 	= comment;
---
+++ mainParser/commentParser/Token.py
@@ -0,0 +1,6 @@
+class Token:
+	def __init__(self, TokenId, content):
+		self.TokenId 	= TokenId;
+		self.content 	= content;
+	
+    
