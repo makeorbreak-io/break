f06049d6027b6bc057db6cdb483d6564d10d65ed
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-09-09 12:36:40 +0100

    Github plugin done

--- plugins/github.py
+++ plugins/github.py
@@ -7,7 +7,10 @@ class IssueColection:
     def __init__(self, tag, content = None, comment = None, assignees = None):
         self.tag = tag
         self.comment = comment
-        self.content = content
+        if content is None:
+            self.content = []
+        else:
+            self.content = content
         if assignees is None:
             self.assignees = []
         else:
@@ -22,11 +25,6 @@ data = input('Password: ')
 password = str(data)
 print('\n')
 
-'''metodo de usa variaveis de ambiente para inicializar password e username'''
-'''SE VAIRAVEIS AMBIENTE NAO ESTAO SET PEDIR PARA CORRER SCRIPT
-SE ESTIVEREM SET ATRIBUIR AO USERNAME E PASSWORD'''
-
-
 session = requests.Session()
 session.auth = (username, password)
 
@@ -35,83 +33,66 @@ if (parser.Parse()):
     issues = session.get('https://api.github.com/repos/portosummerofcode/break/issues')
     githubIssues = issues.json()
 
-    for i, gitIssue in enumerate(githubIssues):
+    if issues.status_code != 200:
+        print("ERROR!")
+        exit(1)
+
+    i = 0
+    while (i < len(githubIssues)):
         if githubIssues[i]['state'] == 'closed':
             githubIssues.pop(i)
+        else:
+            i += 1
 
-
-    '''TODO collections nao estao a funcionar'''
     issuesCollections = []
-    i = 0
-    while (i <= len(parser.issues)):
-        j = i + 1
-        decrement = 0
-        while (j <= len(parser.issues)):
-            if parser.issues[i].comment == parser.issues[j].comment and parser.issues[i].tag == parser.issues[j].tag and parser.issues[i].assignees == parser.issues[j].assignees:
-                decrement += 1
-                match = False
-                c1 = parser.issues[i].file + ':' + parser.issues[i].lineno
-                for collection in issuesCollections:
-                    if parser.issues[i].comment == collection.comment:
-                        match = True
-                        f1 = False
-                        for content in collection.content:
-                            if c1 == content:
-                                f1 = True
-                        if f1 == False:
-                            content = content + '\n' + c1
-                if match == False:
-                    col = IssueColection(parser.issues[i].tag, c1, parser.issues[i].comment, parser.issues[i].assignees)
-                    issuesCollections.append(col)
-                parser.issues.pop(i)
-            j += 1
-        i += 1
-        i -= decrement
-        print(issuesCollections)
-
-
-    for issue in parser.issues:
+    for i, issue1 in enumerate(parser.issues):
         match = False
-        for i, gitIssue in enumerate(githubIssues):
-            if githubIssues[i]['title'] == issue.comment:
+        c1 = issue1.file + ':' + issue1.lineno
+        for collection in issuesCollections:
+            if issue1.comment == collection.comment and issue1.tag == collection.tag:
                 match = True
-                url = 'https://api.github.com/repos/portosummerofcode/break/issues' + '/' + str(githubIssues[i]['number'])
-                content = issue.file + ':' + str(issue.lineno)
-                r = session.patch(url, json = {'body':content, 'labels':[issue.tag], 'assignees':issue.assignees})
-                print(r.text)
-                githubIssues.pop(i)
+                f1 = False
+                for cont in collection.content:
+                    if c1 == cont:
+                        f1 = True
+                if f1 == False:
+                    collection.content.append(c1)
+                f1 = False
+                for ass in  issue1.assignees:
+                    for ass2 in collection.assignees:
+                        if ass == ass2:
+                            f1 = True
+                    if f1 == False:
+                        collection.assignees.append(ass)
+                    f1 = False
         if match == False:
-            content = issue.file + ':' + str(issue.lineno)
-            r = session.post('https://api.github.com/repos/portosummerofcode/break/issues',
-                json = {'title':issue.comment, 'body':content, 'labels':[issue.tag], 'assignees':issue.assignees})
-            print(r.text)
+            col = IssueColection(issue1.tag, [c1], issue1.comment, issue1.assignees)
+            issuesCollections.append(col)
+
+
+
+
 
     for collection in issuesCollections:
+        string = ''
+        for content in collection.content:
+            string += content + '\n'
         match = False
         for i, gitIssue in enumerate(githubIssues):
-            if githubIssues[i]['title'] == collection.comment:
+            if githubIssues[i]['title'] == collection.comment and githubIssues[i]['labels'][0]['name'] == collection.tag:
                 match = True
                 url = 'https://api.github.com/repos/portosummerofcode/break/issues' + '/' + str(githubIssues[i]['number'])
-                r = session.patch(url, json = {'body':collection.content})
-                print(r.text)
+                r = session.patch(url, json = {'body':string, 'assignees':collection.assignees})
                 githubIssues.pop(i)
         if match == False:
             r = session.post('https://api.github.com/repos/portosummerofcode/break/issues',
-                json = {'title':collection.comment, 'body':collection.content, 'labels':[collection.tag], 'assignees':collection.assignees})
-            print(r.text)
+                json = {'title':collection.comment, 'body':string, 'labels':[collection.tag], 'assignees':collection.assignees})
 
 
     for gitIssue in githubIssues:
         url = 'https://api.github.com/repos/portosummerofcode/break/issues' + '/' + str(gitIssue['number'])
-        print(url)
         r = session.patch(url, json = {'state':'closed'})
-        print(r.text)
 
 
 else:
     print("Parse failed")
-
-
-
-
-    '''Issue('TODO', 'main.cpp', 10, 'Make method to parse strings')'''
