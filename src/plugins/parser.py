a8c4ad6d12e03d1b747fd9f2983dd060b47301cb
João Silva <j.pedro004@gmail.com>
2017-09-10 00:53:44 +0100

    Restructure folder

---
+++ src/plugins/parser.py
@@ -0,0 +1,87 @@
+class Issue:
+    def __init__(self, tag, file = None, lineno = None, comment = None, assignees = None):
+        self.tag = tag
+        self.file = file
+        self.lineno = lineno
+        self.comment = comment
+        if assignees is None:
+            self.assignees = []
+        else:
+            self.assignees = assignees
+
+class Parser:   
+    def __init__(self, filepath):
+        self.tokens = open(filepath, 'r').read().split()
+        self.index = 0
+        self.issues = []
+
+    def NextToken(self):
+        if self.index < len(self.tokens):
+            token = self.tokens[self.index]            
+            self.index += 1
+            return token
+        return None
+
+    def Parse(self):
+        token = self.NextToken()
+        return self.Tag(token)
+
+    def Tag(self, token):
+        if token == ":tag":
+            token = self.NextToken()
+            if token is not None:
+                issue = Issue(token)
+                token = self.NextToken()
+                return self.Parameter(token, issue)
+            else:
+                return False
+        elif token == None:
+            return True
+
+    def Parameter(self, token, issue):
+        if token == ":file":
+            token = self.NextToken()
+            if token is not None:
+                issue.file = token
+                token = self.NextToken()
+                return self.Parameter(token, issue)
+            else:
+                return False
+        elif token == ":lineno":
+            token = self.NextToken()
+            if token is not None:
+                issue.lineno = token
+                token = self.NextToken()
+                return self.Parameter(token, issue)
+            else:
+                return False
+        elif token == ":assignee":
+            token = self.NextToken()
+            assignee = ""
+            while token != None and not token.startswith(":"):
+                assignee += " " + token
+                token = self.NextToken()
+            if assignee == "":
+                return False
+            else:
+                issue.assignees.append(assignee.strip())
+                return self.Parameter(token, issue)
+        elif token == ":comment":
+            token = self.NextToken()
+            comment = ""
+            while token != None and not token.startswith(":"):
+                comment += " " + token
+                token = self.NextToken()
+            if comment == "":
+                return False
+            else:
+                issue.comment = comment.strip()
+                return self.Parameter(token, issue)
+        else:            
+            if issue.comment is not None:
+                self.issues.append(issue)                
+                return self.Tag(token)
+            else:
+                return False
+        
+
---
+++ src/plugins/trello.py
@@ -0,0 +1,127 @@
+import requests
+
+class IssueCollection:
+    def __init__(self, tag, content = None, comment = None, assignees = None):
+        self.tag = tag
+        self.comment = comment
+        if content is None:
+            self.content = []
+        else:
+            self.content = content
+        if assignees is None:
+            self.assignees = []
+        else:
+            self.assignees = assignees
+
+class Plugin:
+    def __init__(self, token, key, issues, board, task_list, done_list, tags):
+        self.token = token
+        self.key = key
+        self.issues = issues
+        self.board = board
+        self.task_list = task_list
+        self.done_list = done_list
+        self.tags = tags
+
+    def run(self):
+        issue_collections = []
+        for i, issue in enumerate(self.issues):
+            match = False
+            issue_file_lineno = issue.file + ":" + str(issue.lineno)
+            for collection in issue_collections:
+                if issue.comment == collection.comment and issue.tag == collection.tag:
+                    match = True
+                    
+                    for collection_file_lineno in collection.content:
+                        found_line = False
+                        if issue_file_lineno == collection_file_lineno:
+                            found_line = True
+                    if not found_line:
+                        collection.content.append(issue_file_lineno)
+                        
+                    for issue_assignee in issue.assignees:
+                        found_assignee = False
+                        for collection_assignee in collection.assignees:
+                            if issue_assignee == collection_assignee:
+                                found_assignee = True
+                        if not found_assignee:
+                            collection.assignees.append(issue_assignee)
+
+            if not match:
+                collection = IssueCollection(issue.tag, [issue_file_lineno], issue.comment, issue.assignees)
+                issue_collections.append(collection)
+
+        r = requests.get("https://api.trello.com/1/lists/" + self.task_list + "/cards?key=" + self.key + "&token=" + self.token);
+        if r.status_code < 200 or r.status_code > 299:
+            return False
+        task_cards = r.json()
+
+        r = requests.get("https://api.trello.com/1/lists/" + self.done_list + "/cards?key=" + self.key + "&token=" + self.token);
+        if r.status_code < 200 or r.status_code > 299:
+            return False
+        done_cards = r.json()
+
+        to_add = [collection for collection in issue_collections if not any(collection.comment == card["name"] and collection.tag == card["labels"][0]["name"] for card in task_cards) and not any(collection.comment == card["name"] and collection.tag == card["labels"][0]["name"] for card in done_cards)]
+        # to_update = [collection for collection in issue_collections if collection not in to_add]
+        to_delete = [card for card in task_cards if not any(card["name"] == collection.comment and card["labels"][0]["name"] == collection.tag for collection in issue_collections)]
+
+        to_update = []
+        for collection in issue_collections:
+            for card in task_cards:
+                if collection.comment == card["name"] and collection.tag == card["labels"][0]["name"]:
+                    to_update.append((collection, card))
+
+        for collection in to_add:
+            r = requests.post("https://api.trello.com/1/cards", json = {"key":self.key, "token":self.token, "idList":self.task_list, "name":collection.comment})
+            if r.status_code < 200 or r.status_code > 299:
+                continue
+            card_id = r.json()["id"]
+            
+            color = self.tags[collection.tag]
+            requests.post("https://api.trello.com/1/cards/" + card_id + "/labels", json = {"key":self.key, "token":self.token, "color":color, "name":collection.tag})
+
+            for assignee in collection.assignees:
+                r = requests.get("https://api.trello.com/1/types/" + assignee + "?key=" + self.key + "&token=" + self.token);
+                if r.status_code < 200 or r.status_code > 299:
+                    continue
+                id = r.json()["id"]
+                requests.post("https://api.trello.com/1/cards/" + card_id + "/idMembers", json = {"key":self.key, "token":self.token, "value":id})
+
+            r = requests.post("https://api.trello.com/1/cards/" + card_id + "/checklists", json = {"key":self.key, "token":self.token, "name":"Occurrences"})
+            if r.status_code < 200 or r.status_code > 299:
+                continue
+            checklist_id = r.json()["id"]
+            for content in collection.content:
+                requests.post("https://api.trello.com/1/checklists/" + checklist_id + "/checkItems", json = {"key":self.key, "token":self.token, "name":content})
+
+        for collection, card in to_update:
+            card_id = card["id"]
+            for member in card["idMembers"]:
+                requests.delete("https://api.trello.com/1/cards/" + card_id + "/idMembers/" + member, json = {"key":self.key, "token":self.token})
+
+            for assignee in collection.assignees:
+                r = requests.get("https://api.trello.com/1/types/" + assignee + "?key=" + self.key + "&token=" + self.token);
+                if r.status_code < 200 or r.status_code > 299:
+                    continue
+                id = r.json()["id"]
+                requests.post("https://api.trello.com/1/cards/" + card_id + "/idMembers", json = {"key":self.key, "token":self.token, "value":id})
+
+            for checklist in card["idChecklists"]:
+                requests.delete("https://api.trello.com/1/cards/" + card_id + "/checklists/" + checklist, json = {"key":self.key, "token":self.token})
+
+            r = requests.post("https://api.trello.com/1/cards/" + card_id + "/checklists", json = {"key":self.key, "token":self.token, "name":"Occurrences"})
+            if r.status_code < 200 or r.status_code > 299:
+                continue
+            checklist_id = r.json()["id"]
+            for content in collection.content:
+                requests.post("https://api.trello.com/1/checklists/" + checklist_id + "/checkItems", json = {"key":self.key, "token":self.token, "name":content})
+
+        for card in to_delete:
+            card_id = card["id"]
+            print(card_id)
+            r = requests.put("https://api.trello.com/1/cards/" + card_id, json = {"key":self.key, "token":self.token, "idList":self.done_list})
+            print(r.text)
+            
+        return True
+
+        
