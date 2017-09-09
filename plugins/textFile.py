f0cba8724dae44d9ae4b6a8f48f44db322110615
margaridaviterbo <margaridaviterbo@hotmail.com>
2017-09-09 17:32:08 +0100

    Text file pulgin with plugin class

--- plugins/textFile.py
+++ plugins/textFile.py
@@ -2,36 +2,54 @@ import requests
 
 from parser import Parser
 
-def print_issues(issues, text_file):
-    for issue in issues:
-        text_file.write("[" + issue.file + ":" + str(issue.lineno) + "]" + " " + issue.comment)
-        if len(issue.assignees) > 0:
-            text_file.write(" (" + ", ".join(issue.assignees) + ")\n")
-        else:
-            text_file.write("\n")
-    text_file.write("\n")
-    return
+class Plugin:
+    def __init__(self, filepath, issues):
+        self.filepath = filepath
+        self.issues = issues
 
+    def print_issues(self, issues, text_file):
+        for issue in issues:
+            text_file.write("[" + issue.file + ":" + str(issue.lineno) + "]" + " " + issue.comment)
+            if len(issue.assignees) > 0:
+                text_file.write(" (" + ", ".join(issue.assignees) + ")\n")
+            else:
+                text_file.write("\n")
+        text_file.write("\n")
+        return
 
-parser = Parser('../test/intermediate.txt')
+    def run(self):
+        todos = []
+        fixmes = []
+        notes = []
+        for issue in self.issues:
+            if issue.tag == 'TODO':
+                todos.append(issue)
+            elif issue.tag == 'FIXME':
+                fixmes.append(issue)
+            elif issue.tag == 'NOTE':
+                notes.append(issue)
+
+        try:
+            text_file = open(self.filepath, "w")
+        except IOError:
+            return False
+        with text_file:
+            if len(todos) > 0:
+                text_file.write("TODO:\n")
+                self.print_issues(todos, text_file)
+            if len(fixmes) > 0:
+                text_file.write("FIXME:\n")
+                self.print_issues(fixmes, text_file)
+            if len(notes) > 0:
+                text_file.write("NOTE:\n")
+                self.print_issues(notes, text_file)
+            text_file.close()
+        return True
+
+'''
+test here
+parser = Parser('../test/intermediate2.txt')
 parser.Parse()
-
-todos = []
-fixmes = []
-notes = []
-for issue in parser.issues:
-    if issue.tag == 'TODO':
-        todos.append(issue)
-    elif issue.tag == 'FIXME':
-        fixmes.append(issue)
-    elif issue.tag == 'NOTE':
-        notes.append(issue)
-
-text_file = open("../Tasks.txt", "w")
-text_file.write("TODO:\n")
-print_issues(todos, text_file)
-text_file.write("FIXME:\n")
-print_issues(fixmes, text_file)
-text_file.write("NOTE:\n")
-print_issues(notes, text_file)
-text_file.close()
+t = Plugin("../Tasks.txt", parser.issues)
+t.run()
+'''
