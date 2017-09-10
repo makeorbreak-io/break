e8f9c6276cb1342cd82cc0f5fa231a97fffd7caa
João Silva <j.pedro004@gmail.com>
2017-09-10 03:00:19 +0100

    Add test files

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+// TODO(joaopsilva22): Figure out if this is needed
+#include <string.h> 
+#include <stdio.h>
+
+int
+main() {
+    // NOTE: Maybe say hi to something else
+    printf("Hello, World!");
+
+    // FIXME(joaopsilva22, margaridaviterbo): Missing return value!
+}
